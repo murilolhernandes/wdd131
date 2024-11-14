28b4e01c267428c83a1933b14a27ed234fab92df
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2024-11-13 20:42:27 -0700

    finished event-practice activity

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,11 +1,21 @@
-const input = document.querySelector("#favchap");
-const button = document.querySelector("button");
-const list = document.querySelector("#list");
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
+// const input = document.querySelector("#favchap");
+// const button = document.querySelector("button");
+// const list = document.querySelector("#list");
+// const li = document.createElement("li");
+// const deleteButton = document.createElement("button");
 
-li.textContent = input.value;
-deleteButton.textContent = "❌";
+// li.textContent = input.value;
+// deleteButton.textContent = "❌";
 
-li.append(deleteButton);
-list.append(li);
+// li.append(deleteButton);
+// list.append(li);
+
+const buttonElement = document.getElementById("submitButton");
+
+function copyInput(event) {
+  console.log(event);
+  const inputElement = document.getElementById("favchap");
+  const outputElement = document.getElementById("list");
+  outputElement.innerHTML = inputElement.value;
+}
+buttonElement.addEventListener("click", copyInput);
