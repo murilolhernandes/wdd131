5dc0317762666b7f6c1d59be152012be4ebad173
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2024-11-14 22:12:06 -0700

    final touches added

--- scripts/temples.js
+++ scripts/temples.js
@@ -12,9 +12,11 @@ lastModified.innerHTML = `Last Modification: ${modification.toLocaleString()}`;
 const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
 const title = document.querySelector(".title");
+const header = document.querySelector("header");
 
 hamButton.addEventListener("click", () => {
   navigation.classList.toggle("open");
   hamButton.classList.toggle("open");
   title.classList.toggle("open");
+  header.classList.toggle("open");
 });
