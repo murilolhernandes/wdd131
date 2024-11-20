57d0584b2d4462d61743520455c24378382729c7
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2024-11-20 15:50:04 -0700

    finished assignment

--- scripts/place.js
+++ scripts/place.js
@@ -13,21 +13,28 @@ const windSpeed = 6;
 const temperatureToday = 22;
 const conditionToday = "Cloudy";
 const humidityToday = 79;
-const windChillToday = calculateWindChill();
 const windchill = document.querySelector("#windchill");
 const condition = document.querySelector("#condition");
 const temperature = document.querySelector("#temperature");
 const humidity = document.querySelector("#humidity");
 const wind = document.querySelector("#wind");
-temperature.innerHTML = `<strong>Temperature:</strong> ${temperatureToday}℉`;
-condition.innerHTML = `<strong>Condition:</strong> ${conditionToday}`;
-humidity.innerHTML = `<strong>Humidity:</strong> ${humidityToday}%`;
-wind.innerHTML = `<strong>Wind Speed:</strong> ${windSpeed} mph`;
-windchill.innerHTML = `<strong>Wind Chill:</strong> ${windChillToday.toFixed(1)}℉`;
-
-
-
 
 function calculateWindChill() {
   return 35.74 + 0.6215 * temperatureToday - 35.75 * windSpeed**0.16 + 0.4275 * temperatureToday * windSpeed**0.16;
 }
+
+if (windSpeed > 3 && temperatureToday <= 50) {
+  windChillToday = calculateWindChill().toFixed(1);
+} else {
+  windChillToday = "N/A";
+}
+
+temperature.innerHTML = `<strong>Temperature:</strong> ${temperatureToday}℉`;
+condition.innerHTML = `<strong>Condition:</strong> ${conditionToday}`;
+humidity.innerHTML = `<strong>Humidity:</strong> ${humidityToday}%`;
+wind.innerHTML = `<strong>Wind Speed:</strong> ${windSpeed} mph`;
+windchill.innerHTML = `<strong>Wind Chill:</strong> ${windChillToday}℉`;
+
+
+
+
