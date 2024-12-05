66f63d2748d4375325c367e35bcf8c533fa8931f
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2024-12-05 09:28:12 -0700

    added an * and gave the color red to point out a required field

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,4 @@
 const today = new Date();
 document.getElementById("currentYear").innerHTML = `©️ ${today.getFullYear()} | Murilo Luiz Hernandes | Idaho`;
 document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
+
