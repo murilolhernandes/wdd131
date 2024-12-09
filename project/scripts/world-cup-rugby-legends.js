1b69d1fafe82968cb85a6fbca48caa8459705314
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2024-12-09 11:08:28 -0700

    rough html done

---
+++ project/scripts/world-cup-rugby-legends.js
@@ -0,0 +1,3 @@
+const today = new Date();
+document.getElementById("currentYear").innerHTML = `©️ ${today.getFullYear()} | Murilo Luiz Hernandes | Idaho`;
+document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
