b0db851c92d6aceff072b45c8033d4415edde061
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2024-12-10 10:42:06 -0700

    syncing before my risky test

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -19,3 +19,34 @@ document.getElementById("springboks").addEventListener("click", function() {
 document.getElementById("formpage").addEventListener("click", function() {
   location.href = "formpage.html";
 });
+
+const images = document.querySelectorAll(".fade-image");
+const scrollThreshold = [50, 200, 2800, 4300];
+
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+  images.forEach((image, index) => {
+    const threshold = scrollThreshold[index];
+    const fadeOutThreshold = threshold + (image.offsetHeight / 2);
+    if (scrollPosition >= fadeOutThreshold) {
+      // image.style.opacity = 0;
+      image.classList.add("fade-out");
+    } else {
+      // image.style.opacity = 1;
+      image.classList.remove("fade-out");
+    }
+  });
+});
+
+// const scrollThreshold = window.innerHeight * 0.5;
+
+// function handleScroll() {
+//   images.forEach(image => {
+//     const imageTop = image.getBoundingClientRect().top;
+//     if (imageTop < scrollThreshold) {
+//       image.classList.add("fade-in");
+//     }
+//   });
+// }
+
+// window.addEventListener("scroll", handleScroll);
