27033e93cb648c625b6bcdf0e480f1c8255260c4
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2024-12-10 18:17:31 -0700

    change of plans, I will make a slide photo album instead

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -20,33 +20,221 @@ document.getElementById("formpage").addEventListener("click", function() {
   location.href = "formpage.html";
 });
 
-const images = document.querySelectorAll(".fade-image");
-const scrollThreshold = [50, 200, 2800, 4300];
+// const images = document.querySelectorAll(".fade-image");
+// const scrollThreshold = [50, 200, 2800, 4300];
 
-window.addEventListener("scroll", () => {
-  const scrollPosition = window.scrollY;
-  images.forEach((image, index) => {
-    const threshold = scrollThreshold[index];
-    const fadeOutThreshold = threshold + (image.offsetHeight / 2);
-    if (scrollPosition >= fadeOutThreshold) {
-      // image.style.opacity = 0;
-      image.classList.add("fade-out");
-    } else {
-      // image.style.opacity = 1;
-      image.classList.remove("fade-out");
-    }
-  });
-});
-
-// const scrollThreshold = window.innerHeight * 0.5;
-
-// function handleScroll() {
-//   images.forEach(image => {
-//     const imageTop = image.getBoundingClientRect().top;
-//     if (imageTop < scrollThreshold) {
-//       image.classList.add("fade-in");
+// window.addEventListener("scroll", () => {
+//   const scrollPosition = window.scrollY;
+//   images.forEach((image, index) => {
+//     const threshold = scrollThreshold[index];
+//     const fadeOutThreshold = threshold + (image.offsetHeight / 2);
+//     if (scrollPosition >= fadeOutThreshold) {
+//       // image.style.opacity = 0;
+//       image.classList.add("fade-out");
+//     } else {
+//       // image.style.opacity = 1;
+//       image.classList.remove("fade-out");
 //     }
 //   });
-// }
+// });
 
-// window.addEventListener("scroll", handleScroll);
+let heroContainer = document.querySelector(".hero-container");
+
+
+let images = [
+  {
+    src: "images/springboks.webp",
+    srcset: "images/springboks-large.webp",
+    media: "(min-width: 1000px)",
+    loading: "lazy"
+  },
+  {
+    src: "images/allblacks.webp",
+    srcset: "images/allblacks-large.webp",
+    media: "(min-width: 1000px)",
+    loading: "lazy"
+  },
+  {
+    src: "images/wallabies.webp",
+    srcset: "images/wallabies-large.webp",
+    media: "(min-width: 1000px)",
+    loading: "lazy"
+  },
+  {
+    src: "images/redandwhites.webp",
+    srcset: "images/redandwhites-large.webp",
+    media: "(min-width: 1000px)",
+    loading: "lazy"
+  }
+];
+
+// let currentImageIndex = 0;
+
+setNewImage(images, 0, "picture-container1", "image1");
+// setNewImage(images, 1, "picture-container2", "image2");
+// setNewImage(images, 2, "picture-container3", "image3");
+// setNewImage(images, 3, "picture-container4", "image4");
+
+function setNewImage(array, index, className1, className2) {
+  let newPicture = document.createElement("picture");
+  newPicture.classList.add(className1);
+  heroContainer.appendChild(newPicture);
+
+  let pictureContainer = document.querySelector("." + className1);
+
+  let newSource = document.createElement("source");
+  newSource.srcset = array[index].srcset;
+  newSource.media = array[index].media;
+
+  pictureContainer.appendChild(newSource);
+
+  let newImage = document.createElement("img");
+  newImage.src = array[index].src;
+  newImage.loading = array[index].loading; // ?? 'lazy';
+  newImage.classList.add("hero-image", "fade-in", className2);
+  pictureContainer.appendChild(newImage);
+
+  // if (newImage.loading === undefined) {
+  //   newImage.loading = "eager";
+  // } else {
+  //   newImage.loading = array[index].loading;
+  // }
+  // newPicture.appendChild(newImage);
+}
+
+function addImage(array, index) {
+  index = index;
+  heroContainer.a(heroImage);
+  setNewImage(array, index);
+}
+
+window.addEventListener("scroll", () => {
+  let scrollPosition = window.scrollY;
+  let windowHeight = window.innerHeight;
+  let heroImage = document.querySelector(".hero-image");
+})
+
+// let scrollDirection = null;
+
+// let heroImage = document.querySelector(".hero-image");
+
+// heroContainer.removeChild(heroImage);
+
+// let newSource = document.createElement("source");
+// newSource.srcset = images[currentImageIndex].srcset;
+// newSource.media = images[currentImageIndex].media;
+
+// heroContainer.appendChild(newSource);
+
+// let newImage = document.createElement("img");
+// newImage.src = images[currentImageIndex].src;
+// newImage.loading = images[currentImageIndex].loading // ?? 'lazy';
+// newImage.classList.add("hero-image", "fade-in");
+// heroContainer.appendChild(newImage);
+
+// newImage.addEventListener("load", () => {
+//   heroImage.classList.add("fade-out");
+//   heroImage.addEventListener("transitionend", () => {
+//     heroImage.classList.remove("fade-out");
+//     heroImage.classList.remove("fade-in");
+//     heroImage.parentNode.replaceChild(newImage, heroImage);
+//     heroImage = newImage;
+//   });
+// })
+// heroContainer.appendChild(newImage);
+
+
+// let isScrolling = false;
+// let scrollTimeout = null;
+
+// window.addEventListener("scroll", () => {
+//   if (!isScrolling) {
+//     isScrolling = true;
+//     let scrollPosition = window.scrollY;
+//     let windowHeight = window.innerHeight;
+
+//     if (scrollPosition >= windowHeight / 2 ) {
+//       currentImageIndex++;
+//       if (currentImageIndex >= images.length) {
+//         currentImageIndex = 0;
+//       }
+      
+//       let newImage = new Image();
+//       newImage.src = images[currentImageIndex].src;
+//       console.log("New image source:", newImage.src);
+//       newImage.classList.add("hero-image", "fade-in");
+//       console.log("New image classes:", newImage.classList);
+//       newImage.addEventListener("load", () => {
+//         heroImage.classList.add("fade-out");
+//         console.log("Hero image classes:", heroImage.classList);
+//         heroImage.addEventListener("transitionend", () => {
+//           console.log("Transitionend event fired");
+//           // heroImage.classList.add("fade-in");
+//           heroImage.classList.remove("fade-out");
+//           newImage.classList.remove("fade-in");
+//           heroContainer.replaceChild(newImage, heroImage);
+//           heroImage = newImage;
+//           console.log("Hero image source:", heroImage.src);
+
+//           let existingSource = heroContainer.querySelector("source");
+
+//           if (existingSource) {
+//             existingSource.srcset = images[currentImageIndex].srcset;
+//             existingSource.media = images[currentImageIndex].media;
+//           }
+//           else {
+//             let newSource = document.createElement("source");
+//             newSource.srcset = images[currentImageIndex].srcset;
+//             newSource.media = images[currentImageIndex].media;
+//             heroContainer.insertBefore(newSource, heroImage);
+//           }
+
+//           isScrolling = false;
+//         });
+//       });
+//     }
+//   }
+// });
+
+// window.addEventListener("scroll", () => {
+//   if (!isScrolling) {
+//     isScrolling = true;
+//     console.log("Scroll event fired");
+//     newImage.addEventListener("load", () => {
+//       heroImage.classList.add("fade-out");
+//       heroImage.addEventListener("transitionend", () => {
+//         heroImage.classList.remove("fade-out");
+//         heroImage.classList.remove("fade-in");
+//         heroImage.parentNode.replaceChild(newImage, heroImage);
+//         heroImage = newImage;
+//       });
+//     });
+//     clearTimeout(scrollTimeout);
+//     scrollTimeout = setTimeout(() => {
+//       currentImageIndex++;
+//       console.log("Scroll event fired");
+
+//       if (currentImageIndex >= images.length) {
+//         currentImageIndex = 0;
+//         isScrolling = false;
+//       }
+
+//       console.log("Scroll event fired");
+
+//       newSource.srcset = images[currentImageIndex].srcset;
+//       newImage.src = images[currentImageIndex].src;
+//       newImage.classList.add("fade-in");
+//       console.log("Scroll event fired");
+
+
+//       setTimeout(() => {
+//         console.log("timeout event fired");
+
+//         newImage.classList.remove("fade-out");
+//         console.log("timeout event fired");
+
+//         isScrolling = false;
+//       }, 1500); // adjust the timeout value to be slightly longer than the transition duration
+//     }, 500); // adjust the timeout value to control the debouncing
+//   }
+// });
