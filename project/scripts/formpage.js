a2d5d2caa7b7bcb31242e7baa5857550ccece89e
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2024-12-13 12:22:31 -0700

    finished form functions

--- project/scripts/formpage.js
+++ project/scripts/formpage.js
@@ -2,12 +2,32 @@ const today = new Date();
 document.getElementById("currentYear").innerHTML = `©️ ${today.getFullYear()} | Murilo Luiz Hernandes | Idaho`;
 document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
 
-function daysUntilWorldCup() {
-  const rugbyWorldCupDate = new Date("2027-10-01T00:00:00.000Z");
-  const today = new Date();
-  const timeDiff = rugbyWorldCupDate.getTime() - today.getTime() - 1;
-  const daysLeft = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-  return daysLeft;
+function choose() {
+  document.querySelector(".choose").addEventListener("submit", (e) => {
+    e.preventDefault();
+    document.querySelector(".choose").classList.add("hide");
+
+    let selectOption = document.querySelector("input[name=choose]:checked").value;
+    selectOption = "." + selectOption;
+
+    if (selectOption === ".question") {
+      let question = document.querySelector(selectOption);
+      question.classList.remove("hide");
+
+    } else if (selectOption === ".share") {
+        let share = document.querySelector(selectOption);
+        share.classList.remove("hide");
+    } else if (selectOption === ".updates") {
+        let updates = document.querySelector(selectOption);
+        updates.classList.remove("hide");
+    } else if (selectOption === ".suggestion") {
+        let suggestions = document.querySelector(selectOption);
+        suggestions.classList.remove("hide");
+    } else if (selectOption === ".newsletter") {
+        let newsletter = document.querySelector(selectOption);
+        newsletter.classList.remove("hide");
+    }
+  })
 }
 
-document.getElementById("countdown").innerHTML = daysUntilWorldCup() + " days until the next Rugby World Cup"
+choose();
