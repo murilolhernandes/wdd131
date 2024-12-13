c61d4c09766e8baee11c4526ca6b77690225d451
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2024-12-13 08:49:43 -0700

    finished the forms. on to css

---
+++ project/scripts/formpage.js
@@ -0,0 +1,13 @@
+const today = new Date();
+document.getElementById("currentYear").innerHTML = `©️ ${today.getFullYear()} | Murilo Luiz Hernandes | Idaho`;
+document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
+
+function daysUntilWorldCup() {
+  const rugbyWorldCupDate = new Date("2027-10-01T00:00:00.000Z");
+  const today = new Date();
+  const timeDiff = rugbyWorldCupDate.getTime() - today.getTime() - 1;
+  const daysLeft = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  return daysLeft;
+}
+
+document.getElementById("countdown").innerHTML = daysUntilWorldCup() + " days until the next Rugby World Cup"
