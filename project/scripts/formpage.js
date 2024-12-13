c2b361ea373cd6d09092235ff0a015bd75f7b2f3
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2024-12-13 14:48:45 -0700

    finished localStorage and JS for formspage

--- project/scripts/formpage.js
+++ project/scripts/formpage.js
@@ -5,29 +5,56 @@ document.querySelector("#lastModified").textContent = `Last Modification: ${docu
 function choose() {
   document.querySelector(".choose").addEventListener("submit", (e) => {
     e.preventDefault();
-    document.querySelector(".choose").classList.add("hide");
 
     let selectOption = document.querySelector("input[name=choose]:checked").value;
     selectOption = "." + selectOption;
 
     if (selectOption === ".question") {
       let question = document.querySelector(selectOption);
-      question.classList.remove("hide");
-
+      if (question) {
+        submit(question);
+      }
     } else if (selectOption === ".share") {
         let share = document.querySelector(selectOption);
-        share.classList.remove("hide");
+        if (share) {
+          submit(share);
+        }
     } else if (selectOption === ".updates") {
         let updates = document.querySelector(selectOption);
-        updates.classList.remove("hide");
+        if (updates) {
+          submit(updates);
+        }
     } else if (selectOption === ".suggestion") {
         let suggestions = document.querySelector(selectOption);
-        suggestions.classList.remove("hide");
+        if (suggestions) {
+          submit(suggestions);
+        }
     } else if (selectOption === ".newsletter") {
         let newsletter = document.querySelector(selectOption);
-        newsletter.classList.remove("hide");
+        if (newsletter)
+        submit(newsletter);
     }
+    document.querySelector(".choose").classList.add("hide");
   })
+}
+
+function submit(form) {
+  form.classList.remove("hide");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    form.classList.add("hide");
+    document.querySelector(".thank-you").classList.remove("hide");
+    storage();
+  });
+}
+
+
+
+function storage() {
+  let counter = localStorage.getItem("submitCounter");
+  counter = counter ? parseInt(counter) + 1 : 1;
+  localStorage.setItem("submitCounter", counter);
+  document.getElementById("message").textContent = `Forms Submitted: ${counter}`
 }
 
 choose();
